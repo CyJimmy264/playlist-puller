64fd9442c80f9913f6e85aab4fefde17643f6c32
mveynberg <mv@cj264.ru>
2018-11-08 19:59:12 +0500

    Fill gemspec with initial data

--- spec/playlist/puller_spec.rb
+++ spec/playlist/puller_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Playlist::Puller do
   it "has a version number" do
     expect(Playlist::Puller::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
